c0e667645b54ef64e8931f6af9ecb0e8f00b4bc7
MJ <chenmengchieh2012@gmail.com>
2021-08-27 16:27:22 +0800

    add monitoring html and controller

---
+++ ESPMonitor/monitor.py
@@ -0,0 +1,53 @@
+import json
+
+HaveMonitors = {}
+OnlineMonitors = {}
+class MonitorService:
+    def __init__(self):
+        with open('espConfig.json', newline='') as espConfig:
+            configData = json.load(espConfig)
+            _ipTable = configData['espTable']
+            self.monitorIds = _ipTable.keys()
+
+    def setOnline(self,Id):
+        global OnlineMonitors
+        OnlineMonitors[Id] = True
+
+    def getOnline(self):
+        ret = {}
+        for monitorId in self.monitorIds:
+            if monitorId in OnlineMonitors:
+                ret[monitorId] = True
+            else:
+                ret[monitorId] = False
+        return ret
+
+
+    def setExist(self,Id, targetId,result):
+        global HaveMonitors
+        if Id in HaveMonitors:
+            HaveMonitors[Id][targetId] = result
+        else:
+            HaveMonitors[Id] ={}
+            HaveMonitors[Id][targetId] = result
+
+    def getExist(self):
+        global HaveMonitors
+        ret = {}
+        for monitorId in self.monitorIds:
+            ret[monitorId] = {}
+            for cacheKey in self.monitorIds:
+                if monitorId == cacheKey:
+                    continue
+                if monitorId in HaveMonitors:
+                    if cacheKey in HaveMonitors[monitorId]:
+                        ret[monitorId][cacheKey] = HaveMonitors[monitorId][cacheKey]
+                    else:
+                         ret[monitorId][cacheKey] = False
+                else:
+                    ret[monitorId][cacheKey] = False
+        return ret
+
+
+
+
